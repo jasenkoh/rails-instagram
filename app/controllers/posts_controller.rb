464c01bf4f89321c3f4e657a9f7cc3368c058d32
jasenko <jasenko.hadziomeragic@virginpulse.com>
2015-07-02 16:50:57 +0200

    Initial commit with photo upload

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+# Posts controller
+class PostsController < ApplicationController
+  def index
+    @post = Post.all
+  end
+
+  def new
+    @post = Post.create
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
+end
