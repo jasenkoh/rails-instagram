02146fec1ae9db2d0aa5644bd82607c78532ac14
jasenko <jasenko.hadziomeragic@virginpulse.com>
2015-07-02 20:06:33 +0200

    add edit and delete picture

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 # Posts controller
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -9,13 +11,42 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+      flash[:success] = 'Successfully created new post.'
+      redirect_to posts_path
+    else
+      flash[:alert] = 'Error creating new post!'
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to(posts_path)
+    else
+      flash[:alert] = 'Error updating post!'
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to(posts_path)
   end
 
   private
 
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
